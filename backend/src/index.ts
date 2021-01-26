a433c3709ccc1b3f132b793a5383efbc4cc37e51
niels <niels>
2021-01-27 00:50:19 +0100

    Refactor && add validation

---
+++ backend/src/index.ts
@@ -0,0 +1,47 @@
+import * as bodyParser from 'body-parser'
+import express from 'express'
+import helmet from 'helmet'
+
+import {
+	getUsers,
+	getUser,
+	createUser,
+	updateUser,
+	deleteUser,
+} from './controllers/UserController';
+import {
+	getPost,
+	getPosts,
+	createPost,
+	updatePost,
+	deletePost,
+} from './controllers/PostController';
+import {
+	validate,
+	createUserSchema,
+	updateUserSchema,
+	createPostSchema,
+	updatePostSchema,
+} from './middlewares/ValidateMiddleware';
+
+const app = express()
+app.use(helmet());
+app.use(bodyParser.json())
+
+// Users
+app.get('/users', getUsers);
+app.get('/users/:id', getUser);
+app.post('/users', validate(createUserSchema), createUser);
+app.put('/users/:id', validate(updateUserSchema), updateUser);
+app.delete('/users/:id', deleteUser);
+
+// Posts
+app.get('/posts', getPosts);
+app.get('/posts/:id', getPost);
+app.post('/posts', validate(createPostSchema), createPost);
+app.put('/posts/:id', validate(updatePostSchema), updatePost);
+app.delete('/posts/:id', deletePost);
+
+app.listen(3000, () =>
+	console.log('🚀 Server ready at: http://localhost:3000'),
+)
---
+++ backend/src/middlewares/ValidateMiddleware.ts
@@ -0,0 +1,67 @@
+import { body, validationResult, ValidationChain } from 'express-validator';
+import { Request, Response, NextFunction } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const validate = (schemas: ValidationChain[]) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		await Promise.all(schemas.map((schema) => schema.run(req)));
+
+		const result = validationResult(req);
+		if (result.isEmpty()) {
+			return next();
+		}
+
+		const errors = result.array();
+		return res.status(422).json(errors);
+	};
+};
+
+export const createUserSchema = [
+	body('name', 'Name is required').notEmpty(),
+	body('email', 'Email is required')
+		.notEmpty()
+		.custom((value) => {
+			if (!value) return true;
+			return prisma.user
+				.findUnique({
+					where: {
+						email: value,
+					},
+				})
+				.then((user) => {
+					if (user) {
+						return Promise.reject('E-mail already in use');
+					}
+				});
+		}),
+];
+
+export const updateUserSchema = [
+	body('name', 'Name is required').notEmpty(),
+	body('email', 'Email is required').custom((value, { req: { params } }) => {
+		if (!value) return true;
+		return prisma.user
+			.findUnique({
+				where: {
+					email: value,
+				},
+			})
+			.then((user) => {
+				if (user && params?.id && Number(params.id) !== user.id) {
+					return Promise.reject('E-mail already in use');
+				}
+			});
+	}),
+];
+
+export const createPostSchema = [
+	body('title', 'Title is required').notEmpty(),
+	body('authorId', 'Author is required').notEmpty(),
+];
+
+export const updatePostSchema = [
+	body('title', 'Title is required').notEmpty(),
+	body('authorId', 'Author is required').notEmpty(),
+];
